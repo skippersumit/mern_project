16241646fd3da7410c07220bda8f5e058aa29863
skippersumit <skipper.sumit@gmail.com>
2020-02-01 21:48:17 +0530

    mongoDB connected

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+//connect Database
+connectDB();
+
 app.get("/", (req, res) => res.send("API Running"));
+
 const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
